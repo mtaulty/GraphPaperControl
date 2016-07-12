5606a7a265e360023920cfc6772d59e0ee13cde4
Mike Taulty <mtaulty@hotmail.com>
2016-07-12 22:14:19 +0100

    Initial version

---
+++ GraphPaperControl/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿namespace GraphPaperControl
+{
+  using System;
+  using System.Collections.Generic;
+  using System.IO;
+  using System.Linq;
+  using System.Runtime.InteropServices.WindowsRuntime;
+  using Windows.ApplicationModel;
+  using Windows.ApplicationModel.Activation;
+  using Windows.Foundation;
+  using Windows.Foundation.Collections;
+  using Windows.UI.Xaml;
+  using Windows.UI.Xaml.Controls;
+  using Windows.UI.Xaml.Controls.Primitives;
+  using Windows.UI.Xaml.Data;
+  using Windows.UI.Xaml.Input;
+  using Windows.UI.Xaml.Media;
+  using Windows.UI.Xaml.Navigation;
+
+  sealed partial class App : Application
+  {
+    public App()
+    {
+      this.InitializeComponent();
+    }
+    protected override void OnLaunched(LaunchActivatedEventArgs e)
+    {
+      Frame rootFrame = Window.Current.Content as Frame;
+
+      if (rootFrame == null)
+      {
+        // Create a Frame to act as the navigation context and navigate to the first page
+        rootFrame = new Frame();
+
+        // Place the frame in the current Window
+        Window.Current.Content = rootFrame;
+      }
+
+      if (e.PrelaunchActivated == false)
+      {
+        if (rootFrame.Content == null)
+        {
+          rootFrame.Navigate(typeof(MainPage), e.Arguments);
+        }
+        // Ensure the current window is active
+        Window.Current.Activate();
+      }
+    }
+  }
+}
---
+++ GraphPaperControl/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GraphPaperControl
+{
+  using Windows.UI.Xaml.Controls;
+
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
+    {
+      this.InitializeComponent();
+    }
+  }
+}
